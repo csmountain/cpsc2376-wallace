a5eb6cb3af5f5f714c3149caee5881474707e89a
Carter Wallace <carter.wallace@gmail.com>
2025-02-25 20:51:17 +0000

    forgot to commit when working on 1a. will do so continuing. Bugfixing done for 1a

---
+++ practice/practice05/practice05_1/problem1a.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <iterator>
+
+int main()
+{
+    std::vector<int> numbers;
+    int number;
+    int count = 1;
+
+    while (true)
+    {
+        std::cout << "Integer " << count << " (press any non-numerical key and enter to stop the program): ";
+        if (std::cin >> number)
+        {
+            numbers.push_back(number);
+            count++;
+            std::cout << "\n";
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    // Clear the error state
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    // Prints the numbers using iterators
+    std::cout << "\nYou entered: ";
+    for (auto it = numbers.begin(); it != numbers.end(); ++it)
+    {
+        std::cout << *it;
+        if (std::next(it) != numbers.end())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    // Find and prints the sum using an iterator loop
+    int sumEven = 0;
+    for (auto it = numbers.begin(); it != numbers.end(); ++it)
+    {
+        if (*it % 2 == 0)
+        {
+            sumEven += *it;
+        }
+    }
+    std::cout << "\nSum of even numbers: " << sumEven << std::endl;
+
+    return 0;
+}
